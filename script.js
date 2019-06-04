54c7a1af8ea4846a2a4dbeef8575d877517da1e3
lucasPA <lukasz.patalan@gmail.com>
2019-06-04 22:36:55 +0200

    menu changes

--- script.js
+++ script.js
@@ -8,13 +8,21 @@ button.onclick = function () {
         //gdy jest otwarty chcemy schować
         content.className = "";
         button.innerHTML = '<img src="img/hamburger.png">'
-
+        // document.body.style.background = "rgba(255, 255, 255, 0)"
 
 
     } else {
         //gdy zamknięty, chcemy rozwinąć: wystarczy dać class "open"
         content.className = "open"
         button.innerHTML = '<img src="img/eks.png">'
-        //button.innerHTML = "show Less";
+
+        // document.body.style.background = "rgba(0,0,0,0.3)"
     }
 };
+
+var clickMenu = document.getElementById("hiddenMenu")
+var content2 = document.getElementById("hiddenMenu");
+clickMenu.onclick = function () {
+    content2.className = "";
+    button.innerHTML = '<img src="img/hamburger.png">'
+}
