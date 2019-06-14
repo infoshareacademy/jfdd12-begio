3b9fc0f8e243f53754912d4c43696cf77ce9eac7
Andrzej Zaborski <andrzejzab@gmail.com>
2019-06-14 16:24:21 +0200

    Hero freezes of space press

--- GAME/begioAnimation/script.js
+++ GAME/begioAnimation/script.js
@@ -8,9 +8,18 @@ body.append(canvas)
 
 const ctx = canvas.getContext('2d')
 
+document.addEventListener('keydown', heroJump)
+
 const image = new Image()
 
 image.src = 'running_man.png'
+
+let spacePreesed = false
+
+function heroJump(event) {
+    spacePreesed = event.code === "Space"
+    console.log(spacePreesed)
+}
 
 let FRAME_X = 0
 let FRAME_Y = 0
@@ -23,20 +32,22 @@ image.onload = function () {
     const FRAME_HEIGHT = IMAGE_HEIGHT / 5
 
     setInterval(() => {
-        console.log('Hello!')
         ctx.clearRect(0, 0, 600, 600)
         ctx.drawImage(image, FRAME_X * FRAME_WIDTH, FRAME_Y * FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT, 0, 0, FRAME_WIDTH, FRAME_HEIGHT)
-        console.log(FRAME_X)
-        if (FRAME_X < 5) {
-            FRAME_X++
+        if (spacePreesed) {
+            FRAME_X += 0
+            setTimeout(() => spacePreesed = false, 1000)
         } else {
-            FRAME_X = 0
-            if (FRAME_Y < 4) {
-                FRAME_Y++
+            if (FRAME_X < 5) {
+                FRAME_X++
             } else {
-                FRAME_Y = 0
+                FRAME_X = 0
+                if (FRAME_Y < 4) {
+                    FRAME_Y++
+                } else {
+                    FRAME_Y = 0
+                }
             }
-
         }
     }, 30);
 }
