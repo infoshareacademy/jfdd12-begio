efe27292edfb2c92587d5932acd34d864b01aab9
TekMal <k.kochanowska6@gmail.com>
2019-06-17 22:39:10 +0200

    wip: added new function for dreate one object in main loop, deleted unnecessary comments

--- GAME/script.js
+++ GAME/script.js
@@ -1,6 +1,4 @@
-//wymiary
 const WIDTH = 987
-
 const HEIGHT = 673
 const BASE_HEIGHT = 88
 const BASE_WIDTH = 987
@@ -11,50 +9,41 @@ const MANHOLL_HEIGHT = 44
 const DRESIK_WIDTH = 100
 const DRESIK_HEIGHT = 144
 
-//canvas
+let backgroundX = 0
+let baseX = 0
+let baseY = HEIGHT - BASE_HEIGHT
+let manhollX = 400
+let dresikX = 300
+let dresikX2 = 800
+let dresikY = HEIGHT - BASE_HEIGHT - 120
+let backgroundImage
+let baseImage
+let messageImage
+
+
 const canvas = document.createElement('canvas')
 canvas.setAttribute('height', `${HEIGHT}px`)
 canvas.setAttribute('width', `${WIDTH}px`)
 const body = document.querySelector('body')
 body.append(canvas)
 const ctx = canvas.getContext('2d')
-//
-
-let backgroundImage
-let baseImage
-let secondBaseImage
-let messageImage
-let secondBackgroundImage
-let manholl
-let dresik
-
-let backgroundX = 0
-let baseX = 0
-let manhollX = 200
-let dresikX = 100
-let dresikX2 = 100
 
 
-//pozycja, wymiary 3 obrazków-> definiuje kolejność
 backgroundImage = drawImage("background-day.png", 0, 0, WIDTH, HEIGHT, () => {
-  secondBackgroundImage = drawImage("background-day.png", 0, 0, WIDTH, HEIGHT, () => {
     baseImage = drawImage("base.png", 0, (HEIGHT - BASE_HEIGHT), BASE_WIDTH, BASE_HEIGHT, () => {
-      secondBaseImage = drawImage("base.png", 0, (HEIGHT - BASE_HEIGHT), BASE_WIDTH, BASE_HEIGHT, () => {
         messageImage = drawImage("message.png", (WIDTH - MESSAGE_WIDTH) / 2, (HEIGHT - MESSAGE_HEIGHT) / 2, MESSAGE_WIDTH, MESSAGE_HEIGHT)
-          
-      })
     })
   })
-})
-//
-manholl = drawImage("manholl.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, MANHOLL_WIDTH, MANHOLL_HEIGHT)
-dresik = drawImage("dres2.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, DRESIK_WIDTH, DRESIK_HEIGHT)
-dresik2 = drawImage("dres3.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, DRESIK_WIDTH, DRESIK_HEIGHT)
 
-//draw image dla 3 obrazków:
+let secondBackgroundImage = drawImage("background-day.png", 0, 0, WIDTH, HEIGHT)
+let manholl = drawImage("manholl.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, MANHOLL_WIDTH, MANHOLL_HEIGHT)
+let dresik = drawImage("dres2.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, DRESIK_WIDTH, DRESIK_HEIGHT)
+let dresik2 = drawImage("dres3.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, DRESIK_WIDTH, DRESIK_HEIGHT)
+
+
 function drawImage(imageUrl, x, y, w, h, onload = () => {}) {
   const image = new Image()
-  image.src = `assets/sprites/${imageUrl}`
+  image.src = `assets/${imageUrl}`
   image.onload = function () {
     ctx.drawImage(image, x, y, w, h)
     onload()
@@ -75,25 +64,14 @@ canvas.addEventListener('click', () => {
 })
 
 
-//let secondBackgroundImage = drawImage('background-day.png')
-
-
-//------------------------------------------
-
-let lastTime = 0 // need to count delta
-let delta = 0 // lastTime - time -> should be delta
-
-function loop() { // 300
-
-
+function loop() { 
   if (isPlaying) {
     drawBackground()
-    drawBase()
-    drawManholl()
-    drawDresik()
-    drawDresik2()
+    drawSingleObject(baseImage, baseX, baseY)
+    drawSingleObject(manholl, manhollX, baseY)
+    drawSingleObject(dresik, dresikX, dresikY)
+    drawSingleObject(dresik2, dresikX2, dresikY)
     animateBackground()
-    animateBase()
     animatemanholl()
     animateDresik()
     animateDresik2()
@@ -106,20 +84,8 @@ function drawBackground() {
   ctx.drawImage(secondBackgroundImage, backgroundX + WIDTH, 0)
 }
 
-function drawBase() {
-  ctx.drawImage(baseImage, baseX, HEIGHT - BASE_HEIGHT)
-  ctx.drawImage(secondBaseImage, baseX + WIDTH, HEIGHT - BASE_HEIGHT)
-}
-
-function drawManholl() {
-  ctx.drawImage(manholl, manhollX, HEIGHT - BASE_HEIGHT) //do poprawy wyswietla sie zle
-}
-function drawDresik() {
-  ctx.drawImage(dresik, dresikX, HEIGHT - BASE_HEIGHT - 120) //do poprawy wyswietla sie zle
-}
-
-function drawDresik2() {
-  ctx.drawImage(dresik2, dresikX2 + 400, HEIGHT - BASE_HEIGHT - 120) //do poprawy wyswietla sie zle
+function drawSingleObject(image, x, y){
+  ctx.drawImage(image, x, y)
 }
 
 function randomNumber(min, max) {
@@ -129,7 +95,7 @@ function randomNumber(min, max) {
 
 function animateBackground () {
   if (backgroundX < -WIDTH) {
-    backgroundX =0
+    backgroundX = 0
     backgroundX -= 1
   } else {
     backgroundX -= 1
@@ -154,16 +120,7 @@ function animateDresik() {
 function animateDresik2() {
   dresikX2 -= 2
 
-    if (dresikX2 < -500) {
-      dresikX2 = 600
+    if (dresikX2 < -100) {
+      dresikX2 = 1000
     }
-}
-
-function animateBase() {
-  if (baseX < -WIDTH) {
-    baseX = 0
-    baseX -= 2
-  }  else {
-    baseX -= 2
-  }
 }
