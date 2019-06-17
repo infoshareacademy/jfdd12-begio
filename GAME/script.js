9759c9d1e714446e9aeb0e725ca8de2a37bd6822
Kamila Kochanowska <k.kochanowska6@gmail.com>
2019-06-17 16:40:19 +0200

    WIP : refactoring

--- GAME/script.js
+++ GAME/script.js
@@ -8,6 +8,8 @@ const MESSAGE_WIDTH = 300
 const MESSAGE_HEIGHT = 267
 const MANHOLL_WIDTH = 100
 const MANHOLL_HEIGHT = 44
+const DRESIK_WIDTH = 100
+const DRESIK_HEIGHT = 144
 
 //canvas
 const canvas = document.createElement('canvas')
@@ -24,7 +26,13 @@ let secondBaseImage
 let messageImage
 let secondBackgroundImage
 let manholl
+let dresik
+
 let backgroundX = 0
+let baseX = 0
+let manhollX = 200
+let dresikX = 100
+let dresikX2 = 100
 
 
 //pozycja, wymiary 3 obrazków-> definiuje kolejność
@@ -32,14 +40,16 @@ backgroundImage = drawImage("background-day.png", 0, 0, WIDTH, HEIGHT, () => {
   secondBackgroundImage = drawImage("background-day.png", 0, 0, WIDTH, HEIGHT, () => {
     baseImage = drawImage("base.png", 0, (HEIGHT - BASE_HEIGHT), BASE_WIDTH, BASE_HEIGHT, () => {
       secondBaseImage = drawImage("base.png", 0, (HEIGHT - BASE_HEIGHT), BASE_WIDTH, BASE_HEIGHT, () => {
-        messageImage = drawImage("message.png", (WIDTH - MESSAGE_WIDTH) / 2, (HEIGHT - MESSAGE_HEIGHT) / 2, MESSAGE_WIDTH, MESSAGE_HEIGHT, () => {
-          manholl = drawImage("manholl.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, MANHOLL_WIDTH, MANHOLL_HEIGHT)
-        }) //wyrzucic manholl z messageImage
+        messageImage = drawImage("message.png", (WIDTH - MESSAGE_WIDTH) / 2, (HEIGHT - MESSAGE_HEIGHT) / 2, MESSAGE_WIDTH, MESSAGE_HEIGHT)
+          
       })
     })
   })
 })
 //
+manholl = drawImage("manholl.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, MANHOLL_WIDTH, MANHOLL_HEIGHT)
+dresik = drawImage("dres2.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, DRESIK_WIDTH, DRESIK_HEIGHT)
+dresik2 = drawImage("dres3.png", WIDTH / 2, (HEIGHT - BASE_HEIGHT) / 2, DRESIK_WIDTH, DRESIK_HEIGHT)
 
 //draw image dla 3 obrazków:
 function drawImage(imageUrl, x, y, w, h, onload = () => {}) {
@@ -80,13 +90,13 @@ function loop() { // 300
     drawBackground()
     drawBase()
     drawManholl()
-    if (backgroundX < -WIDTH) { // gdy całe pierwsze tło jest poza ekranem x= -601, zerujemy
-      backgroundX = 0
-      backgroundX -= 1
-    } else { //gdy gra jest uruchomiona, zmniejsza przesunięcie o 1
-      backgroundX -= 1
-    }
-
+    drawDresik()
+    drawDresik2()
+    animateBackground()
+    animateBase()
+    animatemanholl()
+    animateDresik()
+    animateDresik2()
   }
   requestAnimationFrame(loop)
 }
@@ -97,10 +107,63 @@ function drawBackground() {
 }
 
 function drawBase() {
-  ctx.drawImage(baseImage, backgroundX, HEIGHT - BASE_HEIGHT)
-  ctx.drawImage(secondBaseImage, backgroundX + WIDTH, HEIGHT - BASE_HEIGHT)
+  ctx.drawImage(baseImage, baseX, HEIGHT - BASE_HEIGHT)
+  ctx.drawImage(secondBaseImage, baseX + WIDTH, HEIGHT - BASE_HEIGHT)
 }
 
 function drawManholl() {
-  ctx.drawImage(manholl, backgroundX, HEIGHT - BASE_HEIGHT - 10) //do poprawy wyswietla sie zle
+  ctx.drawImage(manholl, manhollX, HEIGHT - BASE_HEIGHT) //do poprawy wyswietla sie zle
+}
+function drawDresik() {
+  ctx.drawImage(dresik, dresikX, HEIGHT - BASE_HEIGHT - 120) //do poprawy wyswietla sie zle
+}
+
+function drawDresik2() {
+  ctx.drawImage(dresik2, dresikX2 + 400, HEIGHT - BASE_HEIGHT - 120) //do poprawy wyswietla sie zle
+}
+
+function randomNumber(min, max) {
+  return Math.round((Math.random() * (max-min) + min))
+}
+
+
+function animateBackground () {
+  if (backgroundX < -WIDTH) {
+    backgroundX =0
+    backgroundX -= 1
+  } else {
+    backgroundX -= 1
+  }
+}
+
+function animatemanholl() {
+  manhollX -= 2
+
+    if (manhollX < -100) {
+      manhollX = 1000
+    }
+}
+
+function animateDresik() {
+  dresikX -= 2
+
+    if (dresikX < -100) {
+      dresikX = 1000
+    }
+}
+function animateDresik2() {
+  dresikX2 -= 2
+
+    if (dresikX2 < -500) {
+      dresikX2 = 600
+    }
+}
+
+function animateBase() {
+  if (baseX < -WIDTH) {
+    baseX = 0
+    baseX -= 2
+  }  else {
+    baseX -= 2
+  }
 }
